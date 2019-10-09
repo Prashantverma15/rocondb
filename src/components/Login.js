0f3239513844e098927d76b3f7535fa45337732c
sahil <abc@gmail.com>
2019-10-09 18:16:59 +0530

    inital dashboard

---
+++ src/components/Login.js
@@ -0,0 +1,72 @@
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="login.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </head>
+            <div class="main-bg">
+                    <div class="box-conatiner">
+                        <div id="a">
+                            <div class="circle-ripple"></div>
+                        </div>
+            
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6">
+                                <h1 class="heading-left">For Continue Please Login</h1>
+                            </div>
+                            <div class="col-sm-6 col-md-6">
+                                <div class="wrap-login100">
+                                    <span class="login100-form-title">
+                                        Sign In
+                                    </span>
+                                    <form class="login100-form validate-form p-l-55 p-r-55 p-t-20">
+                                       
+                                        <div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
+                                            <input class="input100" type="text" name="username" placeholder="Username">
+                                            <span class="focus-input100"></span>
+                                        </div>
+                                        <div class="wrap-input100 validate-input" data-validate="Please enter password">
+                                            <input class="input100" type="password" name="pass" placeholder="Password">
+                                            <span class="focus-input100"></span>
+                                        </div>
+                                        <div class="text-right p-t-13 p-b-23">
+                                            <span class="txt1">
+                                                Forgot
+                                            </span>
+                                            <a href="#" class="txt2">
+                                                Username / Password?
+                                            </a>
+                                        </div>
+                                        <div class="container-login100-form-btn">
+                                            <button class="login100-form-btn">
+                                                Sign in
+                                            </button>
+                                        </div>
+                                        <div class="flex-col-c p-t-140 p-b-40">
+                                            <span class="txt1 p-b-9">
+                                                Don’t have an account?
+                                            </span>
+                                            <a href="#" class="txt3">
+                                                Sign up now
+                                            </a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+        <script src="" async defer></script>
+
